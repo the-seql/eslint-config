744cef813fd3743237cb5856b282310f4aac2364
Charles Burgess <consulting@cvburgess.com>
2021-07-21 12:12:19 -0400

    add env info to node

--- node.js
+++ node.js
@@ -1,3 +1,10 @@
 module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
   extends: [require.resolve("./base"), "plugin:node/recommended"],
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
 };
