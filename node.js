5f1d8e0c2a6429fca31628219ab0926a9cc6addd
Andi <49383564+awcrotwell@users.noreply.github.com>
2021-08-10 14:38:00 -0700

    NODE: Prefer ESModules (#2)

--- node.js
+++ node.js
@@ -3,9 +3,9 @@ module.exports = {
     es6: true,
     node: true,
   },
-  extends: [require.resolve("./base"), "plugin:node/recommended"],
+  extends: [require.resolve("./base"), "plugin:node/recommended-module"],
   parserOptions: {
-    sourceType: "module",
     ecmaVersion: 2020,
-  },
+    sourceType: "module",
+  }
 };
