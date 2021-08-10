25b1f97568b2f82467e16fb4d7a9868e59bcca59
Charles Burgess <consulting@cvburgess.com>
2021-08-10 15:53:41 -0400

    Add mobile config

---
+++ mobile.js
@@ -0,0 +1,55 @@
+module.exports = {
+  env: {
+    "react-native/react-native": true,
+  },
+  extends: [
+    require.resolve("./base"),
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:react-native/all",
+  ],
+  overrides: [
+    {
+      env: {
+        "detox/detox": true,
+        "jest": true,
+        "jest/globals": true,
+      },
+      files: ["*.e2e.js"],
+    },
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    sourceType: "module",
+  },
+  plugins: ["react", "react-native"],
+  rules: {
+    "import/order": [
+      "error",
+      {
+        "alphabetize": { order: "asc" },
+        "newlines-between": "always",
+        "pathGroups": [
+          {
+            group: "external",
+            pattern: "react",
+            position: "before",
+          },
+        ],
+        "pathGroupsExcludedImportTypes": ["react"],
+      },
+    ],
+    "react/jsx-sort-props": [
+      "error",
+      { reservedFirst: true, shorthandFirst: true },
+    ],
+    "react/prop-types": "off",
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
