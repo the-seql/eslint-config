7ec6c0547cf5463f4c8b7f468199a758158a27d8
Andi Crotwell <awcrotwell@gmail.com>
2021-08-10 14:26:11 -0700

    node/recommended-module

--- node.js
+++ node.js
@@ -3,18 +3,9 @@ module.exports = {
     es6: true,
     node: true,
   },
-  extends: [require.resolve("./base"), "plugin:node/recommended"],
+  extends: [require.resolve("./base"), "plugin:node/recommended-module"],
   parserOptions: {
     ecmaVersion: 2020,
     sourceType: "module",
-  },
-  rules: {
-    "node/no-unsupported-features/es-syntax": [
-      "error",
-      {
-        ignores: ["modules"],
-        version: ">=13.0.0",
-      }
-    ]
   }
 };
